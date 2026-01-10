58d8bc93b55e06f41309bb9863fd620f08b76d13
Brandon Rajkowski <github@brajkowski.bulc.club>
2026-01-10 10:20:08 -0700

    target Node.js v24 runtime (#692)
    * target Node.js 24 runtime
    
    * use ES modules
    
    * GitHub actions are bumped to latest versions

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,9 @@
 import type { Config } from "jest";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: Config = {
   moduleDirectories: ["node_modules", __dirname],
